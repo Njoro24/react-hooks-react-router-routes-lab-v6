6f02a9655cb0174cfa5751abfd16b6565fd94069
+ <+>
2025-04-20 20:11:46 +0300

    initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import routes from './routes.jsx';
+import routes from './routes';
 
-// Create the router using the routes defined in routes.js
 const router = createBrowserRouter(routes);
 
-// Render the application with RouterProvider
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
-import { useEffect, useState } from "react";
+import NavBar from '../components/NavBar';
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/movies')
+      .then(response => response.json())
+      .then(data => setMovies(data));
+  }, []);
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>Home Page</h1>
+      <div className="movie-list">
+        {movies.map(movie => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
   );
-};
+}
 
 export default Home;
